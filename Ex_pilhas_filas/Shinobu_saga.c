bae4be663774d4eac7fe169b3a5fe560952addcf
Iury Nogueira <ikdn@ic.ufal.br>
2024-02-23 14:20:43 -0300

    shinobu

---
+++ Ex_pilhas_filas/Shinobu_saga.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//Armazena o primeiro item da fila
+typedef struct _stack STACK;
+
+//Armazena o conteudo do no e um ponteiro para o proximo no da fila
+typedef struct _node NODE;
+
+struct _stack
+{
+    NODE* head;
+};
+struct _node
+{
+    int element;
+    NODE* next;
+};
+
+//Cria uma stack com o a head NULL
+STACK* Create_stack();
+
+//Recebe um elemento e cria e retorna um novo node
+// com o elemento passado
+NODE* create_node(int element);
+
+//Verifica se a pilha esta vazia
+int IS_EMPTY(STACK* stack);
+
+//Recebe uma pilha e Retorna o elemento que esta no topo da fila
+int POP(STACK* stack);
+
+//Recebe uma pilha e um inteiro e retorna a nova pilha
+//Adiciona um novo node no topo da pilha
+void PUSH(STACK* stack, int element);
+
+//Recebe a pilha e a operacao a ser feita
+//faz a operacao da calculadora
+void result(STACK* stack, char elementos);
+
+//Recebe uma pilha vazia e quantas strings serao lidas
+//Le as n strings que vao seguir e resolve as expressoes
+void Calculadora(STACK* calculadora, int size);
+
+int main()
+{
+    STACK* calculadora = Create_stack();
+    int k;
+    scanf("%d", &k);
+    Calculadora(calculadora, k);
+    printf("Coordenada 1: %d\n", POP(calculadora));
+    scanf("%d", &k);
+    Calculadora(calculadora, k);
+    printf("Coordenada 2: %d\n", POP(calculadora));
+
+}
+
+STACK* Create_stack()
+{
+    STACK* new_stack = (STACK*)malloc(sizeof(STACK));
+    new_stack->head = NULL;
+    return new_stack;
+}
+
+NODE* create_node(int element)
+{
+    NODE *new_node = (NODE*)malloc(sizeof(NODE));
+    new_node->element = element;
+    new_node->next = NULL;
+   
+    return new_node;
+}
+
+int IS_EMPTY(STACK* stack)
+{
+    return stack->head == NULL;
+    
+}
+
+int POP(STACK* stack){
+    if(IS_EMPTY(stack))  return -1;
+    
+    NODE* aux = stack->head;
+    stack->head = stack->head->next;
+    
+    int element = aux->element;
+    
+    free(aux);
+    
+    return element;
+}
+
+void PUSH(STACK* stack, int element)
+{
+        NODE *new_node = create_node(element);
+        new_node->next = stack->head;
+        stack->head = new_node;
+}
+
+// "51 13 12 * + "
+void result(STACK* stack, char elementos){
+    
+    int num1, num2;
+    
+    switch(elementos){
+        case '+' :
+            num1 = POP(stack);
+            num2 = POP(stack);
+            PUSH(stack, num1 + num2);
+            break;
+        case '-' :
+            num1 = POP(stack);
+            num2 = POP(stack);
+            PUSH(stack, num2 - num1);
+            break;
+        case '*' :
+            num1 = POP(stack);
+            num2 = POP(stack);
+            PUSH(stack, num1 * num2);
+            break;
+        case '/' :
+            num1 = POP(stack);
+            num2 = POP(stack);
+            PUSH(stack, num2 / num1);
+            break;
+    }
+}
+// "51 13 12 * + "
+void Calculadora(STACK* calculadora, int size){
+    
+    char elementos[100];
+   
+    for(int i = 0; i < size; i++){
+        scanf("%s", elementos);
+
+        for(int j = 0; j < strlen(elementos); j++){
+            if(elementos[j] >= '0' && elementos[j] <= '9'){
+                int num = elementos[j] - '0';
+                PUSH(calculadora, num);
+            }else{
+                result(calculadora, elementos[j]);
+            }
+        }
+    }
+}
